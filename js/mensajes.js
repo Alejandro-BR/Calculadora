77d9b8fe9d96dfb097ad78e343f86199c6f810af
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2024-07-14 16:22:40 +0200

    Mensajes al usuario

---
+++ js/mensajes.js
@@ -0,0 +1,19 @@
+// Alejandro Barrionuevo Rosado
+
+/**
+ * Genera un mensaje de error.
+ * @param {string} m - El mensaje de error a mostrar.
+ */
+function error(m) {
+  console.error(m);
+  alert(m);
+}
+
+/**
+ * Genera un mensaje.
+ * @param {string} m - El mensaje a mostrar.
+ */
+function mensaje(m) {
+  console.log(m);
+  alert(m);
+}
